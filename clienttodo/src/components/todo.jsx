11180ea9ef9c57803a541e0de83bbfa4edcf3465
Priyanka Tippa <priyanka@itboons.com>
2021-06-23 20:27:52 -0700

    added input field validation and made few css changes

--- clienttodo/src/components/todo.jsx
+++ clienttodo/src/components/todo.jsx
@@ -3,14 +3,17 @@ import React, { Component } from 'react';
 class Todo extends Component {
     state = { 
         todoItems:[],
-        text:''
+        text:'',
+        error:''
      }
     render() { 
         return ( <div>
-            <h1 style ={{color:"Red"}}> React Todo Application</h1>
+            <h1 style ={{color:"grey"}}> React Todo Application</h1>
             <input type='text' name ='todo' onChange ={this.handleChange} value={this.state.text} placeholder ='enter the text'/>
+            
      
             <button onClick={this.addTodo}>Add Todo</button>
+           <p> <span>{this.state.error}</span></p>
             <ul>
                 {this.state.todoItems.map(todo =>{ return(
                     <li key={todo.id}>{todo}</li>
@@ -23,16 +26,29 @@ class Todo extends Component {
         const newTodo = [
         this.state.text
         ]
+        if(!this.state.text){
+            this.setState({error: 'input required'});
+        } else if(!isNaN(this.state.text)){
+            this.setState({error: 'input cannot be a number'});
+
+        } else{
 
         this.setState(state =>({
             todoItems : this.state.todoItems.concat(newTodo),
-            text:''
+            text:'',
+            error:''
         }));
+    }
     }
 
   
     handleChange = (e) =>{
+
+
         this.setState({text:e.target.value})
+        if(!e.target.value){
+            this.setState({error:''})
+        }
     }
 }
  
